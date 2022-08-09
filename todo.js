121743f33dcd0f04040016c15c213439b4d2e868
pelonomif <pelonomif@gmail.com>
2022-08-09 21:59:06 +0200

    Changed buttons texts to images in html

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+
 //Created variables
 const todoInput = document.querySelector(".to-do-input");
 const todoButton = document.querySelector(".to-do-button");
@@ -13,29 +14,32 @@ function addToDo(event) {
         todoDiv.classList.add ("todo");
 
     const newTodo = document.createElement ("li");
-        newTodo.innerText = "I can";
+        newTodo.innerText = todoInput.value;
         newTodo.classList.add("todo-item");
         todoDiv.appendChild (newTodo);
 
     const completedButton = document.createElement ("button");
-        completedButton.innerText = "Save";
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
         completedButton.classList.add ("complete-btn");
         todoDiv.appendChild (completedButton);
 
-    const trashButton = document.createElement ("button");
-        trashButton.innerText = "Delete";
-        trashButton.classList.add ("trashButton");
-        todoDiv.appendChild (trashButton);
-        //append to list in HTML
-        todoList.appendChild(todoDiv);
-
-    const editButton = document.createElement ("button");
-        editButton.innerText = "Edit";
+        const editButton = document.createElement ("button");
+        editButton.innerHTML= '<i class="fa-solid fa-pen-to-square"></i>';
         editButton.classList.add ("editButton");
         todoDiv.appendChild (editButton);
         //append to list in HTML
         todoList.appendChild(todoDiv);
         
+    const trashButton = document.createElement ("button");
+        trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        trashButton.classList.add ("trashButton");
+        todoDiv.appendChild (trashButton);
+        //append to list in HTML
+        todoList.appendChild(todoDiv);
+        todoInput.value= "";
 
 }
+
+
+
 
