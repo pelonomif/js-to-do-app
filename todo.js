7d9830f2082d20639500348318b51353600eb626
pelonomif <pelonomif@gmail.com>
2022-08-10 12:02:37 +0200

    Indented code accordingly and adjusted the font-size in css

--- todo.js
+++ todo.js
@@ -1,12 +1,12 @@
 
-//Created variables
+//Created variables 
 const todoInput = document.querySelector(".to-do-input");
 const todoButton = document.querySelector(".to-do-button");
 const todoList = document.querySelector(".todo-list");
 
 todoButton.addEventListener("click", addToDo);
 
-//Used a function
+//Used a function to create a div that has buttons for the list
 function addToDo(event) {
 
     
@@ -15,43 +15,51 @@ function addToDo(event) {
 
     const newTodo = document.createElement ("li");
         newTodo.innerText = todoInput.value;
-        newTodo.classList.add("todo-item");
-        todoDiv.appendChild (newTodo);
+            newTodo.classList.add("todo-item");
+                todoDiv.appendChild (newTodo);
 
     const completedButton = document.createElement ("button");
         completedButton.innerHTML = '<i class="fas fa-check"></i>';
-        completedButton.classList.add ("complete-btn");
-        todoDiv.appendChild (completedButton);
+            completedButton.classList.add ("complete-btn");
+                todoDiv.appendChild (completedButton);
 
         const editButton = document.createElement ("button");
         editButton.innerHTML= '<i class="fa-solid fa-pen-to-square"></i>';
-        editButton.classList.add ("editButton");
-        todoDiv.appendChild (editButton);
-        //append to list in HTML
-        todoList.appendChild(todoDiv);
-        
+            editButton.classList.add ("editButton");
+                todoDiv.appendChild (editButton);
+               
     const trashButton = document.createElement ("button");
         trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-        trashButton.classList.add ("trashButton");
-        todoDiv.appendChild (trashButton);
+            trashButton.classList.add ("trashButton");
+                todoDiv.appendChild (trashButton);
         //append to list in HTML
         todoList.appendChild(todoDiv);
         todoInput.value= "";
         
 }
+    //to delete the todo item
+todoList.addEventListener("click", deleteSave);
 
-todoList.addEventListener("click", deleteTask);
-
-function deleteTask(e){
+function deleteSave(e){
 
     const item = e.target;
-
+    
+    
     if (item.classList[0] === "trashButton"){
         const todo =  item.parentElement;
             todo.remove();
 }
+// Added a toggle to add a class to the element
+  
+if (item.classList[0] === "complete-btn"){
+    const todo = item.parentElement;
+       todo.classList.toggle("completed")
 }
 
+}
 
+    
+
+   
 
 
