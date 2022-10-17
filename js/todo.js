01d5e70630fa9b9e581f80a45a71ef849b5489b1
pelonomif <pelonomif@gmail.com>
2022-10-17 11:13:17 +0200

    created todo list using html and css

--- js/todo.js
+++ js/todo.js
@@ -17,20 +17,70 @@ let textarea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
 
 //validating the input fields
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function(e){
     e.preventDefault();
-
+    console.log("button clicked");
+    
     formValidation();
 });
 
 let formValidation = () => {
     if (textInput.value === ""){
-        msg.innerHTML = "Task cannot be blank";
+        msg.innerHTML="Post cannot be blank";
         console.log("failure");
     }
-    else {
+    else{
         console.log("success");
         msg.innerHTML = "";
+
+        acceptData();
     }
 
 };
+
+
+//collecting data from textInput and storing it into localStorage
+let data =[];
+
+let acceptData = (e) => {
+    data.push ({  
+        text: textInput.value,
+        data: dateInput.value,
+        description: textarea.value,
+
+        
+    });
+
+    window.localStorage.setItem ("data", JSON.stringify(data));
+
+    console.log(data);
+
+    createTask();
+};
+
+// creating new tasks using template literal
+let createTask = () => {
+    tasks.innerHTML = "";
+    data.map((x, y) => {
+        return (tasks.innerHTML += `
+        <div id= ${y}>
+            <span class="fw-bold"> ${x.text}</span>
+            <span class="small text-seconday"> ${x.date}</span>
+            <p> ${x.description}</p>
+
+            <span class = "options">
+                <i onClick = "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa fa-pencil-square"></i>
+                <i onClick = "deleteTask(this); createTask(this)" class="fa fa-trash-o"></i>
+            </span>
+        </div>
+        `);
+    });
+
+    resetForm();
+};
+
+let resetForm = () =>{
+    textInput.value= "";
+    dateInput.value= "";
+    textarea.value= "";
+}; 
