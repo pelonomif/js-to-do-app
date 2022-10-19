3c4d8f4a24fbca97e0411ef6fb8d96fc3b8593ce
pelonomif <pelonomif@gmail.com>
2022-10-19 21:54:43 +0200

    Used template literals to collect data from local storage to DOM

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,7 @@
 let form= document.getElementById('form');
 let userInput = document.getElementById('userInput');
 let msg = document.getElementById('msg');
+taskBox =  document.querySelector(".task-box")
 
 //Validating the form
 form.addEventListener("submit", (e) =>{
@@ -24,7 +25,29 @@ let formValidation = () => {
     }
   };
 
-//collect data and use local storage
+  //using template literals to collect data from local storage and adding it
+function showData(){
+  let li = "";
+  data.forEach((todo, id) => {
+    li += `  <li class="task">
+                  <label for="${id}">
+                      <input type="checkbox" id="${id}">
+                      <p>${todo.text}</p>
+                  </label>
+                  <div class="settings">
+                      <i class="fa fa-ellipsis-h"></i>
+                      <ul class="task-menu">
+                          <li><i class="fa fa-pencil">Edit</i></li>
+                          <li><i class="fa fa-trash">Delete</i></li>
+                      </ul>
+                  </div>
+              </li>`;
+  });
+  taskBox.innerHTML = li;
+}
+
+
+//collect data and save it local storage
 let data = [];
 
 let acceptData = () => {
@@ -34,6 +57,8 @@ let acceptData = () => {
 
   localStorage.setItem("data", JSON.stringify(data));
   console.log(data);
+
+  showData();
 
 }
 
